87ec547babe8d6c522ca1f2fdc889d0222b44256
Manaday Mavani <manaday.mavani@investis.com>
2018-10-04 08:11:48 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const uglify = require('gulp-uglify');
+const filter = require('gulp-filter');
+const runSequence = require('run-sequence');
+
+gulp.task('build-clean', function () {
+  del([
+    './dist/**/*'
+  ]);
+});
+
+gulp.task('build-scripts', function () {
+  return gulp.src([
+    './source/scripts/**.js'
+  ])
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts'));
+})
+
+gulp.task('build-styles', function () {
+  return gulp.src([
+    './source/styles/**/*.scss',
+    './source/styles/**/*.css'
+  ])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/styles'))
+});
+
+gulp.task('copy-libs', function () {
+
+  var filterJsCss = filter(['**/*.js', '**/*.css']);
+
+  return gulp.src([
+    './source/libs/**/*'
+  ])
+    .pipe(filterJsCss)
+    .pipe(gulp.dest('./dist/libs'));
+});
+
+gulp.task('build', function () {
+  runSequence('build-clean', ['build-styles', 'build-scripts', 'copy-libs'], function (error) {
+    if (error) {
+      console.error(error);
+    }
+    else {
+      console.log('Build completed successfully.');
+    }
+  })
+})
---
+++ source/scripts/main.js
@@ -0,0 +1,68 @@
+(function (window, axios) {
+  // Generate and set html table on Details section
+  function setTable(repoList) {
+
+    var tableTemplate =
+      '<table>' +
+      '<thead>' +
+      '<th scope="col">Repository</th>' +
+      '<th scope="col">Stars</th>' +
+      '<th scope="col">Language</th>' +
+      '</thead>' +
+      '<tbody class="repos">' +
+      repoList.map(function (repo) {
+        return '<tr class="record">' +
+          '<th class="repo_url" scope="row"><a href="' + repo.html_url + '">' + repo.name + '</a></th>' +
+          '<td class="repo_stars">' + repo.stargazers_count + '</td>' +
+          '<td class="repo_language">' + repo.language + '</td>' +
+          '</tr>';
+      }).join("") +
+      '</tbody>' +
+      '</table>';
+
+    document.querySelector('.details').innerHTML = tableTemplate.trim();
+  }
+
+  function searchRepos() {
+    var userName = document.searchForm.user_name.value;
+    if (userName) {
+      var reqURL = 'https://api.github.com/users/' + userName + '/repos';
+      var reqConfig = { timeout: 3000 };
+
+      // Make http GET request to Github
+      axios.get(reqURL, reqConfig)
+        .then(function (response) {
+          if (response.status === 200) {
+            // set the html table on view
+            setTable(response.data);
+          }
+          else {
+            alert('Invalid response received');
+          }
+        })
+        .catch(function (error) {
+          alert('Error received: ' + error.message);
+        });
+    }
+    else {
+      alert('Invalid or no user name supplied!');
+    }
+  };
+
+  function resetSearch() {
+    document.searchForm.user_name.value = "";
+    document.querySelector('.details').innerHTML = "";
+  }
+
+  function onSearchChange(eventSource) {
+    if (!eventSource.value) {
+      document.querySelector('.details').innerHTML = "";
+    }
+  }
+  
+  // exports
+  window.searchRepos = searchRepos;
+  window.resetSearch = resetSearch;
+  window.onSearchChange = onSearchChange;
+
+})(window, axios);
